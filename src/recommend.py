22ce9cc96d6ff707e1fdc83d2413455b0b05bc93
Martin Pan <wpan03@email.wm.edu>
2021-06-03 13:38:21 -0500

    add more docstrings

--- src/recommend.py
+++ src/recommend.py
@@ -35,13 +35,16 @@ def append_cluster_and_name(df_raw: pd.DataFrame, scale_path: str,
 
 
 def filter_by_cluster(df: pd.DataFrame, cluster_idx: int) -> pd.DataFrame:
-    df_one_cluster = df.query('cluster_label == @cluster_idx').reset_index(drop=True)
+    """Return a filtered dataframe only with observations associated with a specific cluster"""
+    df_one_cluster = df.query('cluster_label == @cluster_idx').reset_index(
+        drop=True)
     logger.debug('Cluster %d has %d observations', cluster_idx,
                  df_one_cluster.shape[0])
     return df_one_cluster
 
 
 def swap_to_right(input_arr: np.ndarray, i: int) -> np.ndarray:
+    """"Swap the first element that equals to i with the element index 0"""
     right_idx = np.where(input_arr == i)[0].item()
     input_arr[[0, right_idx]] = input_arr[[right_idx, 0]]
     return input_arr
@@ -49,9 +52,20 @@ def swap_to_right(input_arr: np.ndarray, i: int) -> np.ndarray:
 
 def closest_k_in_cluster(df_one_cluster: pd.DataFrame,
                          features: List,
-                         k: int = 11) -> np.ndarray:
+                         k: int = 10) -> np.ndarray:
+    """Find the k closest elements for each observation in the dataframe
+
+    Args:
+        df_one_cluster (pd.DataFrame): a filtered dataframe with one specific cluster
+        features (List): features that are used to calculate pairwise distances 
+        k (int, optional): how many closest observations to find. Defaults to 10.
+
+    Returns:
+        closest_k(np.ndarray): 1st element in each row is the target 
+            and the rest of k elements are the index of closest elements
+    """
     dist_mtx = pairwise_distances(df_one_cluster[features])
-    closest_k = np.argsort(dist_mtx)[:, :11]
+    closest_k = np.argsort(dist_mtx)[:, :(k+1)]
     # Get rid of the case where two pokemons have exact same feature and thus mess up the ordering
     for i in range(closest_k.shape[0]):
         if closest_k[i][0] != i:
@@ -60,7 +74,19 @@ def closest_k_in_cluster(df_one_cluster: pd.DataFrame,
 
 
 def get_mapping_to_df(df_one_cluster: pd.DataFrame,
-                      closest_info: np.ndarray) -> pd.DataFrame:
+                      closest_info: np.ndarray,
+                      k: int = 10) -> pd.DataFrame:
+    """Map each element (an integer) in the closest info array to its corresponding Pokemon name
+       and return the mapping result as a pandas Dataframe
+
+    Args:
+        df_one_cluster (pd.DataFrame): a filtered dataframe with one specific cluster
+        closest_info (np.ndarray): the array that describes the closest element for each row 
+        k (int, optional): how many recommendations to make. Defaults to 10.
+
+    Returns:
+        pd.DataFrame: records the k recommendations for each Pokemon
+    """
     int2str = df_one_cluster['name'].to_dict()
 
     # Adapted from: https://stackoverflow.com/questions/16992713/translate-every-element-in-numpy-array-according-to-key
@@ -68,15 +94,17 @@ def get_mapping_to_df(df_one_cluster: pd.DataFrame,
 
     df_info = pd.DataFrame(map_closest_info,
                            columns=['input'] +
-                           [f'rec{i}' for i in range(1, 11)])
+                           [f'rec{i}' for i in range(1, (k+1))])
     return df_info
 
 
 def get_url(name: str) -> str:
+    """Generate a url for a given Pokemon str"""
     return f'https://pokemondb.net/pokedex/{name}'
 
 
 def to_long(df_result: pd.DataFrame) -> pd.DataFrame:
+    """Change the dataframe from wide to long format and sort by input and the_rank column"""
     df_long = df_result.melt(id_vars=['input'],
                              var_name='the_rank',
                              value_name='recommendation')
@@ -87,7 +115,18 @@ def to_long(df_result: pd.DataFrame) -> pd.DataFrame:
 
 
 def merge_display(df_long: pd.DataFrame, df_raw: pd.DataFrame,
-                  display_features: List):
+                  display_features: List) -> pd.DataFrame:
+    """Merge display features into the recommendation dataframe
+       from the raw data
+
+    Args:
+        df_long (pd.DataFrame): the recommendation information
+        df_raw (pd.DataFrame): the raw input with display features for each Pokemon
+        display_features (List): the features that will be added into the recommendation table
+
+    Returns:
+        df_final (pd.DataFrame): recommendation information with additional features attached
+    """
     df_final = pd.merge(df_long,
                         df_raw[display_features],
                         left_on='recommendation',
@@ -100,6 +139,7 @@ def merge_display(df_long: pd.DataFrame, df_raw: pd.DataFrame,
 
 
 def format_clean(df: pd.DataFrame) -> pd.DataFrame:
+    """Add a column with url for each Pokemon and clean the format for some columns"""
     df['learn_more'] = df['recommendation'].apply(get_url)
     df.abilities = df.abilities.str.replace('[^a-zA-Z0-9, -]', '', regex=True)
     df.input = df.input.str.lower()
@@ -110,11 +150,22 @@ def format_clean(df: pd.DataFrame) -> pd.DataFrame:
 def generate_recommendation(df: pd.DataFrame, df_raw: pd.DataFrame,
                             num_clusters: int, features: List, k: int,
                             display_features: List, output_path: str) -> None:
+    """Orchestration function to generate and store recommendations
+
+    Args:
+        df (pd.DataFrame): preproessed dataframe with Pokemon name and cluster attached
+        df_raw (pd.DataFrame): the raw input dataframe
+        num_clusters (int): total number of clusters
+        features (List): features that are used to calculate pairwise distances 
+        k (int): how many recommendations to make
+        display_features (List): the features that will be added into the recommendation table
+        output_path (str): the path to store the recommendation result
+    """
     result = []
     for i in range(0, num_clusters):
         df_one_clus = filter_by_cluster(df, i)
         closet_info = closest_k_in_cluster(df_one_clus, features, k)
-        df_info = get_mapping_to_df(df_one_clus, closet_info)
+        df_info = get_mapping_to_df(df_one_clus, closet_info, k)
         result.append(df_info)
     df_result = pd.concat(result, axis=0)
     df_result_long = to_long(df_result)
